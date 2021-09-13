7298f83f6e7814a57f19b715249ef93094a087af
Ncesecrede <francescopm95@gmail.com>
2021-09-13 16:47:36 +0100

    Boolpress

---
+++ app/Http/Controllers/OldController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class HomeController extends Controller
+{
+    //
+    public function index(){
+
+        $allPosts = Post::all();
+
+        return view('home', compact('allPosts'));
+    }
+}
