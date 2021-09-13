e2cbff9f2333c9cb8cdd11b039cab9cce83784f9
Ncesecrede <francescopm95@gmail.com>
2021-09-13 17:05:34 +0100

    Boolpress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,4 +20,19 @@
         </div>
     </div>
 </div>
+
+<div class="container">
+    <div class="row">
+        @foreach ($allPosts as $post)
+            <div class="col-3 post-container">
+                {{$post->name}}
+                <div class="row">
+                    <div class="image-box">
+                        <img src="{{$post->image}}" alt="">
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 @endsection
